942a4aff67e25af001d68abb924cf4350c9b16bf
rupole <devnull@localhost>
2008-09-13 20:34:03 +0000

    Change IID tests for Py3k

--- win32/test/test_pywintypes.py
+++ win32/test/test_pywintypes.py
@@ -24,9 +24,9 @@ class TestCase(unittest.TestCase):
     def testGUID(self):
         s = "{00020400-0000-0000-C000-000000000046}"
         iid = pywintypes.IID(s)
-        iid2 = pywintypes.IID(buffer(iid), True)
+        iid2 = pywintypes.IID(bytes(iid), True)
         self.assertEquals(iid, iid2)
-        self.assertRaises(ValueError, pywintypes.IID, '00', True) # too short
+        self.assertRaises(ValueError, pywintypes.IID, b'00', True) # too short
         self.assertRaises(TypeError, pywintypes.IID, 0, True) # no buffer
 
 if __name__ == '__main__':
