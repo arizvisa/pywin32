88ad820b580f1722c9a38b7968c2504bc54b7ad6
rupole <devnull@localhost>
2008-09-13 17:42:33 +0000

    Convert module init for Py3k

--- com/win32comext/axcontrol/src/AXControl.cpp
+++ com/win32comext/axcontrol/src/AXControl.cpp
@@ -334,19 +334,8 @@ static struct PyMethodDef axcontrol_methods[]=
 	{ NULL, NULL },
 };
 
-static int AddConstant(PyObject *dict, const char *key, long value)
-{
-	PyObject *oval = PyInt_FromLong(value);
-	if (!oval)
-	{
-		return 1;
-	}
-	int rc = PyDict_SetItemString(dict, (char*)key, oval);
-	Py_DECREF(oval);
-	return rc;
-}
 
-#define ADD_CONSTANT(tok) AddConstant(dict, #tok, tok)
+#define ADD_CONSTANT(tok) if (PyModule_AddIntConstant(module, #tok, tok) == -1) RETURN_ERROR;
 
 static const PyCom_InterfaceSupportInfo g_interfaceSupportData[] =
 {
@@ -366,16 +355,35 @@ static const PyCom_InterfaceSupportInfo g_interfaceSupportData[] =
 };
 
 /* Module initialisation */
-extern "C" __declspec(dllexport) void initaxcontrol()
+extern "C" __declspec(dllexport)
+#if (PY_VERSION_HEX < 0x03000000)
+void initaxcontrol(void)
+#else
+PyObject *PyInit_axcontrol(void)
+#endif
 {
-	char *modName = "axcontrol";
-	PyObject *oModule;
-	// Create the module and add the functions
-	oModule = Py_InitModule(modName, axcontrol_methods);
-	if (!oModule) /* Eeek - some serious error! */
-		return;
-	PyObject *dict = PyModule_GetDict(oModule);
-	if (!dict) return; /* Another serious error!*/
+	PyObject *dict, *module;
+
+#if (PY_VERSION_HEX < 0x03000000)
+#define RETURN_ERROR return;
+	module = Py_InitModule("axcontrol", axcontrol_methods);
+#else
+#define RETURN_ERROR return NULL;
+	static PyModuleDef axcontrol_def = {
+		PyModuleDef_HEAD_INIT,
+		"axcontrol",
+		"A module, encapsulating the ActiveX Control interfaces.",
+		-1,
+		axcontrol_methods
+		};
+	module = PyModule_Create(&axcontrol_def);
+#endif
+
+	if (!module)
+		RETURN_ERROR;
+	dict = PyModule_GetDict(module);
+	if (!dict)
+		RETURN_ERROR;
 
 	// Register all of our interfaces, gateways and IIDs.
 	PyCom_RegisterExtensionSupport(dict, g_interfaceSupportData, sizeof(g_interfaceSupportData)/sizeof(PyCom_InterfaceSupportInfo));
@@ -405,4 +413,8 @@ extern "C" __declspec(dllexport) void initaxcontrol()
 	ADD_CONSTANT(EMBDHLP_CREATENOW); // @const axcontrol|EMBDHLP_CREATENOW|
 	ADD_CONSTANT(EMBDHLP_DELAYCREATE); // @const axcontrol|EMBDHLP_DELAYCREATE|
 	ADD_CONSTANT(OLECREATE_LEAVERUNNING); // @const axcontrol|OLECREATE_LEAVERUNNING|
+
+#if (PY_VERSION_HEX >= 0x03000000)
+	return module;
+#endif
 }
