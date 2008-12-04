60e153a7f3ca6eb6c1d6eebbdc8035692fdb72f2
Mark Hammond <mhammond@skippinet.com.au>
2008-12-04 07:30:55 +0000

    fix syntax error introduced by conversion to py3k syntax

--- com/win32comext/ifilter/demo/filterDemo.py
+++ com/win32comext/ifilter/demo/filterDemo.py
@@ -190,7 +190,7 @@ class FileParser:
 
 def _usage():
     import os
-    print("Usage: %s filename [verbose [dumpbody]]" % (os.path.basename(sys.argv[0]),)))
+    print("Usage: %s filename [verbose [dumpbody]]" % (os.path.basename(sys.argv[0]),))
     print()
     print("Where:-")
     print("filename = name of the file to extract text & properties from")
