fab72b15c64562ba114a6eff5b9030971d45c8d9
Mark Hammond <mhammond@skippinet.com.au>
2008-12-19 02:33:04 +0000

    Modernize exception catching

--- com/win32com/servers/perfmon.py
+++ com/win32com/servers/perfmon.py
@@ -17,12 +17,11 @@ class PerfMonQuery:
 	def Query(self, object, counter, instance = None, machine = None):
 		try:
 			return win32pdhutil.GetPerformanceAttributes(object, counter, instance, machine=machine)
-		except win32pdhutil.error as xxx_todo_changeme:
-			(rc, fn, desc) = xxx_todo_changeme.args
-			raise exception.Exception(desc=desc)
+		except win32pdhutil.error as exc:
+			raise exception.Exception(desc=exc.strerror)
 		except TypeError as desc:
 			raise exception.Exception(desc=desc,scode=winerror.DISP_E_TYPEMISMATCH)
-				
+
 if __name__=='__main__':
 	print("Registering COM server...")
 	register.UseCommandLine(PerfMonQuery)
